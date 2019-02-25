bac57ae337043e46a8b6e79751d3486c25bee153
takuya kikuchi <t.kikuchi.pch@gmail.com>
2019-02-25 10:19:50 +0900

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace stratum_server_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
